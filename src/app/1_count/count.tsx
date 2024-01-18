cf4639025bba6d1713993f02cfee9a6add377dc4
chaiyaphat20 <chaiyaphat.b5922314@gmail.com>
2024-01-19 01:14:14 +0700

    fix

---
+++ src/app/1_count/count.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function Count() {
+  const [count, setCount] = useState(0);
+  const handleAddClick = () => {
+    setCount((v) => v + 1);
+  };
+  return (
+    <div>
+      <h1 className="text-[40px]" data-testid="value-count">
+        {count}
+      </h1>
+      <button data-testid="btn-click" onClick={handleAddClick}>
+        Click
+      </button>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-'use client'
-import { useState } from "react"
+"use client";
+import { useState } from "react";
+import Count from "./1_count/count";
 
 export default function Home() {
-  const [count ,setCount] = useState(0)
-  const handleAddClick = () =>{
-    setCount(v=>v+1)
-  }
   return (
     <div>
-      <h1 className='text-[40px]' data-testid="value-count">{count}</h1>
-      <button  data-testid="btn-click" onClick={handleAddClick}>Click</button>
+      <Count />
     </div>
-  )
+  );
 }
